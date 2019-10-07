9606946a4eced4e43a56db4de27a733a8515f9ad
Jesper Dalsgaard <post@jdanet.dk>
2019-10-07 03:30:29 +0200

    Router scrolling

--- src/router.js
+++ src/router.js
@@ -25,5 +25,8 @@ export default new Router({
       name: "challenge-two",
       component: ChallengeTwo
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
 });
